d644d68db5991908a283ab378e3ad9a7c2ea6bdc
Jeffrey Hurtarte <jhurtarter2@miumg.edu.gt>
2023-04-30 17:13:27 -0600

    Add Clientes

---
+++ ProyectoFinal_BDII/ClientesForm.aspx.vb
@@ -0,0 +1,8 @@
+﻿Public Class ClientesForm
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+End Class
